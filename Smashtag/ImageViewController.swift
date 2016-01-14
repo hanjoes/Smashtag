aa67264ccc38bf1b5b19e133b42e628d97230d5b
hanjoes <hanzhou87@gmail.com>
2016-01-14 15:54:54 +0800

    Implemented auto zoom.

--- Smashtag/ImageViewController.swift
+++ Smashtag/ImageViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 private struct Constants {
     static let defaultMinimumZoomScale: CGFloat = 1
-    static let defaultMaximumZoomScale: CGFloat = 2
+    static let defaultMaximumZoomScale: CGFloat = 2.5
 }
 
 class ImageViewController: UIViewController, UIScrollViewDelegate {
@@ -29,19 +29,11 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     var image: UIImage? {
         didSet {
             if image != nil {
-                let rectSize = view.bounds.size
-                let heightRatio = rectSize.height / image!.size.height
-                let widthRatio = rectSize.width / image!.size.width
-                var ratio: CGFloat = 1
-                if heightRatio < 1 || widthRatio < 1 {
-                    ratio = widthRatio > heightRatio ? widthRatio : heightRatio
-                }
-
-                let rect = CGRect(origin: view.frame.origin, size: CGSize(width: image!.size.width * ratio, height: image!.size.height * ratio))
                 imageView.image = image
-                imageView.drawRect(rect)
                 imageView.sizeToFit()
                 scrollView?.contentSize = imageView.frame.size
+                
+                autoZoom()
             }
         }
     }
@@ -49,12 +41,28 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.addSubview(imageView)
+        autoZoom()
         // Do any additional setup after loading the view.
     }
 
     // MARK: - Private
     
     private var imageView = UIImageView()
+    
+    private func autoZoom() {
+        let rectSize = view.bounds.size
+        let heightRatio = rectSize.height / image!.size.height
+        let widthRatio = rectSize.width / image!.size.width
+        
+        var ratio: CGFloat = 1
+        if heightRatio >= 1 || widthRatio >= 1 {
+            ratio = widthRatio > heightRatio ? widthRatio : heightRatio
+            scrollView.minimumZoomScale = ratio
+            scrollView.maximumZoomScale = ratio * 2.5
+        }
+        scrollView.setZoomScale(ratio, animated: true)
+    }
+    
 
     // MARK: - Delegate For ScrollView
     
