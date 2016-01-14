1569c8e34dbba8bbe8a5c7e818761078e401768e
hanjoes <hanzhou87@gmail.com>
2016-01-14 17:35:11 +0800

    Added TabBarViewController to support multiple sub view controllers.

---
+++ Smashtag/Recents.swift
@@ -0,0 +1,24 @@
+//
+//  Recents.swift
+//  Smashtag
+//
+//  Created by Hanzhou Shi on 1/14/16.
+//  Copyright © 2016 USF. All rights reserved.
+//
+
+import Foundation
+
+class Recents {
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var allHistory: [String] {
+        get { return defaults.objectForKey(Constants.NSUserDefaultKey) as? [String] ?? [] }
+        set { defaults.setObject(newValue, forKey: Constants.NSUserDefaultKey) }
+    }
+    
+    private struct Constants {
+        static let NSUserDefaultKey = "SmashTagRecentsNSUserDefaultKey"
+    }
+
+}
